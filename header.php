e7a36f0674784d5bc69c1f81cd1bfe9a34be4017
ljl22 <lauraj.libby@gmail.com>
2020-02-27 20:46:46 -0500

    Nav styling updates

--- header.php
+++ header.php
@@ -27,7 +27,7 @@
 
         <div class="col-lg-3 d-flex align-items-center">
           <nav>
-            <p><a href="#">Navigation goes here</a></p>
+            <p id="nav1"><a href="#">Navigation goes here</a></p>
           </nav>
         </div>
 
