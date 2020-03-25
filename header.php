272447588f75893e365eea99a1fca79fcb7e8fcf
ljl22 <lauraj.libby@gmail.com>
2020-03-25 19:47:14 -0400

    Add comments to header.php file for ease of use

--- header.php
+++ header.php
@@ -5,9 +5,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
   <title><?php bloginfo('name'); ?></title>
+
+  <!--google fonts----->
   <link href="https://fonts.googleapis.com/css?family=Marck+Script|Poppins&display=swap" rel="stylesheet">
+
   <!-- link our css file -->
   <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
+
   <!--javascript -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="js/Web.js"></script>
