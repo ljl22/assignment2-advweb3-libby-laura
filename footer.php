41877c1f47731d8f9d8cad4b9ad1ad9b7aa6033c
ljl22 <lauraj.libby@gmail.com>
2020-03-24 22:22:47 -0400

    Update to styling of footer.php file

--- footer.php
+++ footer.php
@@ -1,43 +1,44 @@
-<footer>
-  <div class="row">
-      <div class="col-md-4 d-flex align-items-center">
-        <nav>
-            <?php
-              if(has_nav_menu('footer-left')){
-              wp_nav_menu(array('theme_location' => 'footer-left', 'container_class' => 'footer-left-class'));
-            }else{
-              echo "Please select a footer left via the dashboard for best results";
-            }
-            ?>
+  <footer>
+    <div class="row">
 
-        </nav>
-      </div>
-      <div class="col-md-4 d-flex align-items-center">
-        <nav>
-            <?php
-              if(has_nav_menu('footer-middle')){
-              wp_nav_menu(array('theme_location' => 'footer-middle', 'container_class' => 'footer-middle-class'));
-            }else{
-              echo "Please select a footer middle menu via the dashboard for best results";
-            }
-            ?>
+        <div class="col-md-4 d-flex align-items-center">
+          <nav>
+              <?php
+                if(has_nav_menu('footer-left')){
+                wp_nav_menu(array('theme_location' => 'footer-left', 'container_class' => 'footer-left-class'));
+              }else{
+                echo "Please select a footer left via the dashboard for best results";
+              }
+              ?>
+          </nav>
+        </div>
 
-        </nav>
-      </div>
-      <div class="col-md-4 d-flex align-items-center">
-        <nav>
-            <?php
-              if(has_nav_menu('footer-right')){
-              wp_nav_menu(array('theme_location' => 'footer-right', 'container_class' => 'footer-right-class'));
-            }else{
-              echo "Please select a footer right via the dashboard for best results";
-            }
-            ?>
+        <div class="col-md-4 d-flex align-items-center">
+          <nav>
+              <?php
+                if(has_nav_menu('footer-middle')){
+                wp_nav_menu(array('theme_location' => 'footer-middle', 'container_class' => 'footer-middle-class'));
+              }else{
+                echo "Please select a footer middle menu via the dashboard for best results";
+              }
+              ?>
+          </nav>
+        </div>
 
-        </nav>
-      </div>
-  </div>
-</footer>
+        <div class="col-md-4 d-flex align-items-center">
+          <nav>
+              <?php
+                if(has_nav_menu('footer-right')){
+                wp_nav_menu(array('theme_location' => 'footer-right', 'container_class' => 'footer-right-class'));
+              }else{
+                echo "Please select a footer right via the dashboard for best results";
+              }
+              ?>
+          </nav>
+        </div>
+
+    </div>
+  </footer>
 
   <?php wp_footer(); ?>
     <!--<div class="navbar fixed-bottom" style="background-color: #2a7886; color: #ffffff;">
