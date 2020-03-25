425bf0868aa4f066e9504b9450f394f733b0e098
ljl22 <lauraj.libby@gmail.com>
2020-03-24 20:15:47 -0400

    Change code to update assignment 2 comments

--- header.php
+++ header.php
@@ -8,6 +8,7 @@
   <link href="https://fonts.googleapis.com/css?family=Marck+Script|Poppins&display=swap" rel="stylesheet">
   <!-- link our css file -->
   <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
   <?php wp_head(); ?>
 </head>
@@ -18,7 +19,7 @@
       <div class="row">
         <div class="col-lg-9">
           <?php if(get_header_image() == ''){?>
-            <h1><a href="<?php get_home_url();?>"><?php bloginfo('name'); ?>
+            <h1><a href="<?php echo get_home_url();?>"><?php bloginfo('name'); ?>
             </a></h1><?php
             }else{?>
               <a href="<?php get_home_url();?>"><img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height;?>" width="<?php echo get_custom_header()->width;?>" alt="logo" /></a>
--- single.php
+++ single.php
@@ -6,7 +6,7 @@
         while(have_posts()){
           the_post(); ?>
             <div class="col-md-12">
-              <?php the_post_thumbnail('medium'); ?>
+              <?php the_post_thumbnail('large'); ?>
 
               <h2><?php the_title(); ?></h2>
 
