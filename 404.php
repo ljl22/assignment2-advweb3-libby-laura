dca113ed7f7512827fdfe1a46ada0989136ea53d
ljl22 <lauraj.libby@gmail.com>
2020-03-24 22:44:25 -0400

    Create and style 404.php page

--- 404.php
+++ 404.php
@@ -1,7 +1,15 @@
 <?php get_header(); ?>
 
-<h1>404 PAGE</h1>
-
-<p>Sorry, but the page you are looking for can't be found!</p>
+  <div class="container">
+    <div class="row">
+      <main class="col-md-12">
+        <h1>Page Cannot Be Found</h1>
+        <p>Our apologies, but the page you are looking for can't be found!</p>
+        <p>Would you like to return <a href="<?php echo home_url('/');?>">home</a>?</p>
+        <p>If not... Here's a puppy to make you smile.</p>
+        <iframe src="https://giphy.com/embed/47HLuGXkNVrUIHAtQW" width="250" height="250" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/cute-aww-eyebleach-47HLuGXkNVrUIHAtQW">via GIPHY</a></p>
+      </main>
+    </div>
+  </div>
 
 <?php get_footer(); ?>
